dcaadb2d6147377dbf6687843cdb8e182bca34ef
HumaCode <humaidizakaria1998@gmail.com>
2023-11-19 23:43:49 +0700

    perbaikan halaman edit profile

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -2,6 +2,7 @@ import 'package:course_app_flutter/components/app_textfield.dart';
 import 'package:course_app_flutter/components/toolbar.dart';
 import 'package:course_app_flutter/components/user_avatar.dart';
 import 'package:course_app_flutter/config/app_strings.dart';
+import 'package:course_app_flutter/styles/app_color.dart';
 import 'package:flutter/material.dart';
 
 class EditProfilePage extends StatelessWidget {
@@ -14,18 +15,44 @@ class EditProfilePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: Column(
-          children: const [
-            UserAvatar(
-              size: 120,
+          children: [
+            Stack(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: UserAvatar(
+                    size: 120,
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Container(
+                    padding: const EdgeInsets.all(3),
+                    decoration: const BoxDecoration(
+                      color: AppColors.primary,
+                      borderRadius: BorderRadius.all(Radius.circular(6)),
+                    ),
+                    child: const Icon(
+                      Icons.edit,
+                      size: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            AppTextField(hint: AppStrings.firstName),
-            SizedBox(height: 16),
-            AppTextField(hint: AppStrings.lastName),
-            SizedBox(height: 16),
-            AppTextField(hint: AppStrings.phoneNumber),
-            SizedBox(height: 16),
-            AppTextField(hint: AppStrings.location),
-            SizedBox(height: 16),
+            const SizedBox(height: 50),
+            const AppTextField(hint: AppStrings.firstName),
+            const SizedBox(height: 16),
+            const AppTextField(hint: AppStrings.lastName),
+            const SizedBox(height: 16),
+            const AppTextField(hint: AppStrings.phoneNumber),
+            const SizedBox(height: 16),
+            const AppTextField(hint: AppStrings.location),
+            const SizedBox(height: 16),
+            const AppTextField(hint: AppStrings.birthday),
+            const SizedBox(height: 16),
           ],
         ),
       ),
